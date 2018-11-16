e76622df72ba00f0877905f4ff09319332df356b
Nicolas Marzullo <marzullonicolas@gmail.com>
2018-11-16 12:01:05 -0300

    Aun no funciona regular
    Falta ver que pasa cuando J se va de rango

--- src/Generador/Regular.java
+++ src/Generador/Regular.java
@@ -1,9 +1,5 @@
 package Generador;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Regular extends Generador {
 
 	private int gradoPretendido;
@@ -15,23 +11,46 @@ public class Regular extends Generador {
 
 	@Override
 	public void generar() throws Exception {
+		int corte, vueltas = 0, j;
+		boolean impar = false;
 
-		List<Integer> lista = new ArrayList<Integer>();
-		for (int i = 0; i < cantidadNodos; i++) {
-			lista.add(i);
+		if (this.cantidadNodos % 2 != 0 && this.gradoPretendido % 2 != 0) {
+			throw new Exception(
+					"No es posible, para un grafo con una cantidad de nodos impares, generar un grafo regular de grado impar.");
 		}
-		
-		if(this.cantidadNodos %2 != 0 && this.gradoPretendido %2 != 0) {
-			throw new Exception("No es posible, para un grafo con una cantidad de nodos impares, generar un grafo regular de grado impar.");
+
+		if (this.gradoPretendido >= this.cantidadNodos) {
+			throw new Exception(
+					"No se puede generar un grafo regular con el grado mayor o igual a la cantidad de nodos");
 		}
-		
-		if(this.gradoPretendido >= this.cantidadNodos) {
-			throw new Exception("No se puede generar un grafo regular con el grado mayor o igual a la cantidad de nodos");
+
+		if (this.gradoPretendido % 2 == 0) {
+			corte = this.gradoPretendido / 2;
+		} else {
+			corte = (this.gradoPretendido / 2) - 1;
+			impar = true;
 		}
-		
-		
-		
-		
+
+		// El salto siempre es i+1;
+		while (vueltas < corte) {
+			for (int i = 0; i < this.cantidadNodos; i++) {
+				j = vueltas + i + 1;
+				if (j >= this.cantidadNodos) {
+					j =  j - i - vueltas - 1; //Hay que encontrarle la vuelta a esto.
+				}
+				this.matriz.set(i, j, 1);
+			}
+			vueltas++;
+		}
+
+		int corteDeLaCruz = this.cantidadNodos / 2;
+		if (impar) {
+			for (int i = 0; i < corteDeLaCruz; i++) {
+				j = i + corteDeLaCruz;
+				this.matriz.set(i, j, 1);
+			}
+		}
+
 	}
 
 }
--- src/tests/GrafoTests.java
+++ src/tests/GrafoTests.java
@@ -5,15 +5,35 @@ import org.junit.Test;
 import Generador.Generador;
 import Generador.Probabilidad;
 import Generador.RegularAdyacencia;
+import Generador.Regular;
 
 public class GrafoTests {
 
+//	@Test
+//	public void probabilidadTest() {
+//		Generador gen = new RegularAdyacencia(100, 75);
+//		
+//		try {
+//			gen.generar();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		gen.calcularParametrosDelGrafo();
+//		System.out.println(gen);
+//	}
+	
+	
 	@Test
-	public void probabilidadTest() {
-		Generador gen = new RegularAdyacencia(100, 75);
-		gen.generar();
+	public void regularTest() {
+		Generador gen = new Regular(6, 2);
+		try {
+			gen.generar();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		gen.calcularParametrosDelGrafo();
 		System.out.println(gen);
 	}
+	
 }
 
