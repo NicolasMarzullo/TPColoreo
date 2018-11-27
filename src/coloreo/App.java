38ffeee940d029990a8febeabb10e39be36faad0
Nicolas Marzullo <marzullonicolas@gmail.com>
2018-11-27 10:33:36 -0300

    Preparo 10000 corridas

--- src/coloreo/App.java
+++ src/coloreo/App.java
@@ -20,111 +20,111 @@ public class App {
 				GrafoNPND grafoNPND40 = new GrafoNPND("600Nodos_40Ady");
 				
 				long t1 = System.currentTimeMillis();
-				grafoNPND40.colorearSecuencial(1000);
+				grafoNPND40.colorearSecuencial(10000);
 				long t2 = System.currentTimeMillis();
 				System.out.println("Secuencial " + grafoNPND40.cantidadMejorDeColores);
 				System.out.println("Tiempo en ms Secuencial: " + (t2-t1));
 				
 				t1 = System.currentTimeMillis();
-				grafoNPND40.colorearMatula(1000);
+				grafoNPND40.colorearMatula(10000);
 				t2 = System.currentTimeMillis();
 				System.out.println("Matula " + grafoNPND40.cantidadMejorDeColores);
 				System.out.println("Tiempo en ms Matula: " + (t2-t1));
 				
 				t1 = System.currentTimeMillis();
-				grafoNPND40.colorearWheelsPower(1000);
+				grafoNPND40.colorearWheelsPower(10000);
 				t2 = System.currentTimeMillis();
 				System.out.println("Wheels Power " +  grafoNPND40.cantidadMejorDeColores);
 				System.out.println("Tiempo en ms Wheels Power: " + (t2-t1));
 				System.out.println("Fin 40 Adyacencia");
 				
-//				System.out.println("60 Adyacencia");
-//				Generador generador60 = new Porcentaje(600,60);
-//				generador60.generar();
-//				generador60.calcularParametrosDelGrafo();
-//				generador60.escribirEnArchivo("600Nodos_60Ady");
-//				//System.out.println(generador50.toString());
-//				GrafoNPND grafoNPND60 = new GrafoNPND("600Nodos_60Ady");
-//				t1 = System.currentTimeMillis();
-//				grafoNPND60.colorearSecuencial(50);
-//				t2 = System.currentTimeMillis();
-//				System.out.println("Secuencial " + grafoNPND60.cantidadMejorDeColores);
-//				System.out.println("Tiempo en ms Secuencial: " + (t2-t1));
-//				
-//				t1 = System.currentTimeMillis();
-//				grafoNPND60.colorearMatula(50);
-//				t2 = System.currentTimeMillis();
-//				System.out.println("Matula " + grafoNPND60.cantidadMejorDeColores);
-//				System.out.println("Tiempo en ms Matula: " + (t2-t1));
-//				
-//				t1 = System.currentTimeMillis();
-//				grafoNPND60.colorearWheelsPower(50);
-//				t2 = System.currentTimeMillis();				
-//				System.out.println("Wheels Power " +  grafoNPND60.cantidadMejorDeColores);
-//				System.out.println("Tiempo en ms Wheels Power: " + (t2-t1));
-//				System.out.println("Fin 60 Adyacencia");
-//				
-//				System.out.println("90 Adyacencia");
-//				Generador generador90 = new Porcentaje(600,90);
-//				generador90.generar();
-//				generador90.calcularParametrosDelGrafo();
-//				generador90.escribirEnArchivo("600Nodos_90Ady");
-//				//System.out.println(generador90.toString());
-//				GrafoNPND grafoNPND90 = new GrafoNPND("600Nodos_90Ady");
-//				t1 = System.currentTimeMillis();
-//				grafoNPND90.colorearSecuencial(50);
-//				t2 = System.currentTimeMillis();				
-//				System.out.println("Secuencial " + grafoNPND90.cantidadMejorDeColores);
-//				System.out.println("Tiempo en ms Secuencial: " + (t2-t1));
-//				
-//				
-//				t1 = System.currentTimeMillis();
-//				grafoNPND90.colorearMatula(50);
-//				t2 = System.currentTimeMillis();				
-//				System.out.println("Matula " + grafoNPND90.cantidadMejorDeColores);
-//				System.out.println("Tiempo en ms Matula: " + (t2-t1));
-//				
-//				t1 = System.currentTimeMillis();
-//				grafoNPND90.colorearWheelsPower(50);
-//				t2 = System.currentTimeMillis();				
-//				System.out.println("Wheels Power " +  grafoNPND90.cantidadMejorDeColores);
-//				System.out.println("Tiempo en ms Wheels Power: " + (t2-t1));
-//				
-//				System.out.println("Fin 90 Adyacencia");
-//				System.out.println("FIN GRAFOS ALEATORIOS");
-//				
-//				//GRAFOS REGULARES
-//				System.out.println("\nGRAFOS REGULARES");
-//				System.out.println("Regular 50 Adyacencia");
-//				Generador generadorReg50 = new RegularAdyacencia(1000,50);
-//				generadorReg50.generar();
-//				generadorReg50.calcularParametrosDelGrafo();
-//				generadorReg50.escribirEnArchivo("1000Nodos_50Ady");
-//				//System.out.println(generadorReg50.toString());
-//				GrafoNPND grafoNPNDReg50 = new GrafoNPND("1000Nodos_50Ady");
-//				grafoNPNDReg50.colorearSecuencial(1);
-//				System.out.println("Secuencial " + grafoNPNDReg50.cantidadMejorDeColores);
-//				grafoNPNDReg50.colorearMatula(1);
-//				System.out.println("Matula " + grafoNPNDReg50.cantidadMejorDeColores);
-//				grafoNPNDReg50.colorearWheelsPower(1);
-//				System.out.println("Wheels Power " +  grafoNPNDReg50.cantidadMejorDeColores);
-//				
-//				System.out.println("Regular 75 Adyacencia");
-//				Generador generadorReg75 = new RegularAdyacencia(1000,75);
-//				generadorReg75.generar();
-//				generadorReg75.calcularParametrosDelGrafo();
-//				generadorReg75.escribirEnArchivo("1000Nodos_75Ady");
-//				//System.out.println(generadorReg50.toString());
-//				GrafoNPND grafoNPNDReg75 = new GrafoNPND("1000Nodos_75Ady");
-//				grafoNPNDReg75.colorearSecuencial(1);
-//				System.out.println("Secuencial " + grafoNPNDReg75.cantidadMejorDeColores);
-//				grafoNPNDReg75.colorearMatula(1);
-//				System.out.println("Matula " + grafoNPNDReg75.cantidadMejorDeColores);
-//				grafoNPNDReg75.colorearWheelsPower(1);
-//				System.out.println("Wheels Power " +  grafoNPNDReg75.cantidadMejorDeColores);
-//
-//				System.out.println("FIN GRAFOS REGULARES");
-//				
+				System.out.println("60 Adyacencia");
+				Generador generador60 = new Porcentaje(600,60);
+				generador60.generar();
+				generador60.calcularParametrosDelGrafo();
+				generador60.escribirEnArchivo("600Nodos_60Ady");
+				//System.out.println(generador50.toString());
+				GrafoNPND grafoNPND60 = new GrafoNPND("600Nodos_60Ady");
+				t1 = System.currentTimeMillis();
+				grafoNPND60.colorearSecuencial(10000);
+				t2 = System.currentTimeMillis();
+				System.out.println("Secuencial " + grafoNPND60.cantidadMejorDeColores);
+				System.out.println("Tiempo en ms Secuencial: " + (t2-t1));
+				
+				t1 = System.currentTimeMillis();
+				grafoNPND60.colorearMatula(10000);
+				t2 = System.currentTimeMillis();
+				System.out.println("Matula " + grafoNPND60.cantidadMejorDeColores);
+				System.out.println("Tiempo en ms Matula: " + (t2-t1));
+				
+				t1 = System.currentTimeMillis();
+				grafoNPND60.colorearWheelsPower(10000);
+				t2 = System.currentTimeMillis();				
+				System.out.println("Wheels Power " +  grafoNPND60.cantidadMejorDeColores);
+				System.out.println("Tiempo en ms Wheels Power: " + (t2-t1));
+				System.out.println("Fin 60 Adyacencia");
+				
+				System.out.println("90 Adyacencia");
+				Generador generador90 = new Porcentaje(600,90);
+				generador90.generar();
+				generador90.calcularParametrosDelGrafo();
+				generador90.escribirEnArchivo("600Nodos_90Ady");
+				//System.out.println(generador90.toString());
+				GrafoNPND grafoNPND90 = new GrafoNPND("600Nodos_90Ady");
+				t1 = System.currentTimeMillis();
+				grafoNPND90.colorearSecuencial(10000);
+				t2 = System.currentTimeMillis();				
+				System.out.println("Secuencial " + grafoNPND90.cantidadMejorDeColores);
+				System.out.println("Tiempo en ms Secuencial: " + (t2-t1));
+				
+				
+				t1 = System.currentTimeMillis();
+				grafoNPND90.colorearMatula(10000);
+				t2 = System.currentTimeMillis();				
+				System.out.println("Matula " + grafoNPND90.cantidadMejorDeColores);
+				System.out.println("Tiempo en ms Matula: " + (t2-t1));
+				
+				t1 = System.currentTimeMillis();
+				grafoNPND90.colorearWheelsPower(10000);
+				t2 = System.currentTimeMillis();				
+				System.out.println("Wheels Power " +  grafoNPND90.cantidadMejorDeColores);
+				System.out.println("Tiempo en ms Wheels Power: " + (t2-t1));
+				
+				System.out.println("Fin 90 Adyacencia");
+				System.out.println("FIN GRAFOS ALEATORIOS");
+				
+				//GRAFOS REGULARES
+				System.out.println("\nGRAFOS REGULARES");
+				System.out.println("Regular 50 Adyacencia");
+				Generador generadorReg50 = new RegularAdyacencia(1000,50);
+				generadorReg50.generar();
+				generadorReg50.calcularParametrosDelGrafo();
+				generadorReg50.escribirEnArchivo("1000Nodos_50Ady");
+				//System.out.println(generadorReg50.toString());
+				GrafoNPND grafoNPNDReg50 = new GrafoNPND("1000Nodos_50Ady");
+				grafoNPNDReg50.colorearSecuencial(1);
+				System.out.println("Secuencial " + grafoNPNDReg50.cantidadMejorDeColores);
+				grafoNPNDReg50.colorearMatula(1);
+				System.out.println("Matula " + grafoNPNDReg50.cantidadMejorDeColores);
+				grafoNPNDReg50.colorearWheelsPower(1);
+				System.out.println("Wheels Power " +  grafoNPNDReg50.cantidadMejorDeColores);
+				
+				System.out.println("Regular 75 Adyacencia");
+				Generador generadorReg75 = new RegularAdyacencia(1000,75);
+				generadorReg75.generar();
+				generadorReg75.calcularParametrosDelGrafo();
+				generadorReg75.escribirEnArchivo("1000Nodos_75Ady");
+				//System.out.println(generadorReg50.toString());
+				GrafoNPND grafoNPNDReg75 = new GrafoNPND("1000Nodos_75Ady");
+				grafoNPNDReg75.colorearSecuencial(1);
+				System.out.println("Secuencial " + grafoNPNDReg75.cantidadMejorDeColores);
+				grafoNPNDReg75.colorearMatula(1);
+				System.out.println("Matula " + grafoNPNDReg75.cantidadMejorDeColores);
+				grafoNPNDReg75.colorearWheelsPower(1);
+				System.out.println("Wheels Power " +  grafoNPNDReg75.cantidadMejorDeColores);
+
+				System.out.println("FIN GRAFOS REGULARES");
+				
 				//FIN GRAFOS REGULARES
 				
 		} catch (Exception e) {
